aae4b4edbae1fb53d7d21a65e4cea8a7486b1bd3
JHIH-SIOU LI <37164678+pedestrianlove@users.noreply.github.com>
2025-12-16 22:26:01 +0800

    Change return type of darknet_main to int

--- host/examples/darknet.c
+++ host/examples/darknet.c
@@ -398,14 +398,14 @@ void visualize(char *cfgfile, char *weightfile)
     visualize_network(net);
 }
 
-void darknet_main(int argc, char **argv)
+int darknet_main(int argc, char **argv)
 {
     //test_resize("data/bad.jpg");
     //test_box();
     //test_convolutional_layer();
     if(argc < 2){
         fprintf(stderr, "usage: %s <function>\n", argv[0]);
-        return;
+        return 0;
     }
     gpu_index = find_int_arg(argc, argv, "-i", 0);
     if(find_arg(argc, argv, "-nogpu")) {
